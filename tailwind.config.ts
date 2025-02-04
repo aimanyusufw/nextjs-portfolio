3f0918f7e5304090d9a86d695dbb1a03b761894a
aimanyusufw <aimanyusufdev@gmail.com>
2025-02-04 20:55:05 +0700

    add : add new colors variabel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,16 @@ export default {
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        text: "#110304",
+        background: "#fefbfb",
+        primary: "#1b262c",
+        secondary: "#0f4d75",
+        accent: "#3283b8",
+        "dark-text": "#fceeef",
+        "dark-background": "#040101",
+        "dark-primary": "#d3dee4",
+        "dark-secondary": "#8ac7f0",
+        "dark-accent": "#4797cd",
       },
       fontFamily: {
         popins: ["Poppins", "serif"],
