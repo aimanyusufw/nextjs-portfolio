d244ca58f8bb9031ac35d3294e4236b71132303a
aimanyusufw <aimanyusufdev@gmail.com>
2025-02-04 22:17:06 +0700

    ref : remove dark mode colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,16 +13,11 @@ export default {
     },
     extend: {
       colors: {
-        text: "#110304",
-        background: "#fefbfb",
-        primary: "#1b262c",
-        secondary: "#0f4d75",
-        accent: "#3283b8",
-        "dark-text": "#fceeef",
-        "dark-background": "#040101",
-        "dark-primary": "#d3dee4",
-        "dark-secondary": "#8ac7f0",
-        "dark-accent": "#4797cd",
+        text: "#fceeef",
+        background: "#040101",
+        primary: "#d3dee4",
+        secondary: "#8ac7f0",
+        accent: "#4797cd",
       },
       fontFamily: {
         popins: ["Poppins", "serif"],
