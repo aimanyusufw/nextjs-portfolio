b4d77fd32b45f0abe9d84fe8124faa9585c9aed4
aimanyusufw <aimanyusufdev@gmail.com>
2025-02-09 21:26:25 +0700

    add : add dummy data

---
+++ src/data/home.ts
@@ -0,0 +1,30 @@
+export const careerEducation = [
+  {
+    title: "Terbuka University - Bachelor’s Degree",
+    image: "Logo_Universitas_Terbuka.svg.png",
+    role: "Information Systems Student",
+    from: "June 2024",
+    until: "Now",
+  },
+  {
+    title: "Bullion Ecosystem International ",
+    image: "/bullion.png",
+    role: "Server Monitoring Staff",
+    from: "April 2023",
+    until: "Now",
+  },
+  {
+    title: "Bullion Ecosystem International ",
+    image: "/bullion.png",
+    role: "Internship",
+    from: "October 2023",
+    until: "Maret 2024",
+  },
+  {
+    title: "Senior High School 4 Kendal ",
+    image: "/logo-smk-4.png",
+    role: "Student",
+    from: "July 2021",
+    until: "Jun 2024",
+  },
+];
