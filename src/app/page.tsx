d859fcb71c06710d767e451c8d7130b8b8f16180
aimanyusufw <aimanyusufdev@gmail.com>
2025-02-14 19:54:03 +0700

    add : add new portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/Skeleton";
-import { careerEducation, socialMedia } from "@/data/home";
+import { careerEducation, portfolios, socialMedia } from "@/data/home";
+import { slug } from "@/lib/stringConverter";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -77,7 +78,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="py-12">
+      <section className="py-14">
         <div className="container">
           <div className="md:px-4 mb-8 flex gap-4 items-center">
             <div
@@ -142,6 +143,50 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <div className="py-14">
+        <div className="container">
+          <div className="md:px-4 mb-8 md:mb-12 flex gap-4 items-center">
+            <div
+              className="w-0 h-0 
+                  border-t-[10px] border-t-transparent
+                  border-l-[15px] border-l-green-500
+                  border-b-[10px] border-b-transparent"
+            ></div>
+            <h1 className="font-firaCode">
+              <span className="text-blue-400">@Aimanyusuf/portfolio</span> cat
+              myWork.txt
+            </h1>
+          </div>
+          <div className="md:px-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+            {portfolios.map((data, index) => (
+              <div key={index}>
+                <a className="mb-6" href={"portfolio/" + slug(data.title)}>
+                  <video
+                    className="pointer-events-none mx-auto md:h-96 xl:h-64 w-full object-cover object-top rounded-md"
+                    src={data.image}
+                    autoPlay
+                    loop
+                  ></video>
+                </a>
+                <h1 className="text-lg font-medium my-4">{data.title}</h1>
+                <div className="flex mb-4">
+                  {data.tag.map((data) => (
+                    <span
+                      key={data}
+                      className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-lg dark:bg-blue-900 dark:text-blue-300"
+                    >
+                      {data}
+                    </span>
+                  ))}
+                </div>
+                <p className="text-sm leading-relaxed font-medium text-neutral-300">
+                  {data.excrept}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
