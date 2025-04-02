0cb75dc7ae8ee772190312ebc35fe5a3d2b774eb
aimanyusufw <aimanyusufdev@gmail.com>
2025-04-02 14:22:30 +0700

    ref<style> : reafacto style section

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -5,58 +5,117 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 sm:py-32">
       <div className="container">
-        <div className="md:px-4 grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-          <div className="space-y-6 md:space-y-8 mb-10 md:mb-0">
-            <span className="bg-red-100 text-red-800 text-xs md:text-sm lg:text-base font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">
-              About Me
-            </span>
-            <h1 className="font-medium text-4xl md:text-5xl leading-relaxed md:leading-relaxed max-w-sm md:max-w-md">
-              Hello, I&apos;m Aiman Yusuf Wicaksono!
-            </h1>
-            <p className="text-xs md:text-sm leading-relaxed text-neutral-300 max-w-sm">
-              Hello, my name is Aiman, and I&lsquo;m a Software Engineer from
-              Indonesia. I enjoy exploring new experiences in my life.
-            </p>
-            <h3 className="font-medium font-firaCode">Connect With Me ⮧</h3>
-            <div className="flex gap-4">
-              {socialMedia.map((data) => (
-                <Link href={data.url} key={data.name} target="_blank">
-                  <data.icon size={20} className="w-4 md:w-5" />
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="space-y-6 md:space-y-8">
-            <h1 className="text-lg md:text-xl">Career & Education</h1>
-            {careerEducation.map((data, index) => (
-              <div className="flex items-center gap-4" key={index}>
-                <Link href={data.link} target="_blank">
-                  <div className="flex justify-center items-center p-2 rounded-full bg-white w-14 h-14 md:w-16 md:h-16">
-                    <Image
-                      src={data.image}
-                      width={70}
-                      height={70}
-                      alt={data.title}
-                    />
-                  </div>
-                </Link>
-                <div className="">
-                  <h1 className="text-base md:text-lg">{data.title}</h1>
-                  <span className="text-xs md:text-sm text-opacity-60">
-                    {data.role}, {data.from} -{" "}
-                    {data.until == "Now" ? (
-                      <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
-                        {data.until}
-                      </span>
-                    ) : (
-                      data.until
-                    )}
-                  </span>
+        <h2 className="text-base/7 font-semibold text-red-500">About Me</h2>
+        <p className="mt-2 max-w-lg text-balance text-4xl font-semibold tracking-tight sm:text-5xl">
+          Hello, I'm Aiman Yusuf Wicaksono!
+        </p>
+        <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-neutral-900 lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight max-lg:text-center">
+                  Educating & Inspiring Developers
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
+                  I share tech insights and tutorials to help developers grow.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                  <img
+                    className="size-full object-cover object-top"
+                    src="img/tiktok.png"
+                    alt=""
+                  />
                 </div>
               </div>
-            ))}{" "}
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-neutral-900 max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight max-lg:text-center">
+                  Building Solutions, Earning Trust
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
+                  Delivering high-quality, scalable software for global clients
+                </p>
+              </div>
+              <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                <img
+                  className="w-full max-lg:max-w-xs"
+                  src="img/progres.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute inset-px rounded-lg bg-neutral-900"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight max-lg:text-center">
+                  What Clients Say
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
+                  Morbi viverra dui mi arcu sed. Tellus semper adipiscing
+                  suspendisse semper morbi.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
+                <img
+                  className="h-[min(152px,40cqw)] object-cover"
+                  src="img/testimoni.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-neutral-900 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight max-lg:text-center">
+                  Mastering Modern Technologies
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
+                  I build scalable applications with cutting-edge tools.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow">
+                <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-700 shadow-2xl">
+                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                    <div className="-mb-px flex text-sm/6 font-medium text-gray-300">
+                      <div className="border-b border-r border-b-white/40 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                        <img
+                          src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+                          className="w-4 h-4 inline me-2"
+                          alt=""
+                        />
+                        Language.js
+                      </div>
+                      <div className="border-r border-gray-600/10 px-4 py-2">
+                        <img
+                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/768px-Go_Logo_Blue.svg.png"
+                          className="w-6 h-3 inline me-2"
+                          alt=""
+                        />
+                        Experience.go
+                      </div>
+                    </div>
+                  </div>
+                  <div className="px-6 pb-14 pt-6"></div>
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
           </div>
         </div>
       </div>
