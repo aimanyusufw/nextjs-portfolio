54f630602a4cdf1468ada3f3c414d8b73ab88cd4
aimanyusufw <aimanyusufdev@gmail.com>
2025-04-09 15:08:15 +0700

    ref : refacto footer section

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,20 +1,72 @@
-import { socialMedia } from "@/data/home";
-import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-12 bg-neutral-900">
-      <div className="container">
-        <div className="md:px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
-          <h1 className="text-sm">© 2023. Aiman Yusuf Wicaksono.</h1>
-          <div className="flex justify-end gap-4">
-            {socialMedia.map((data) => (
-              <Link href={data.url} key={data.name} target="_blank">
-                <data.icon size={16} />
-              </Link>
-            ))}
+    <footer className="bg-neutral-900">
+      <div className="mx-auto w-full max-w-screen-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:py-8 px-4 py-6">
+          <h1 className="font-popins font-bold text-2xl text-text">AYW.</h1>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 justify-end">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-white">
+                Social Media
+              </h2>
+              <ul className="text-gray-300 font-medium">
+                <li className="mb-4">
+                  <a href="#" className=" hover:underline">
+                    Instagram
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Facebook
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Tiktok
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Linkedin
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-white">
+                Technologies
+              </h2>
+              <ul className="text-gray-300 font-medium">
+                <li className="mb-4">
+                  <a href="#" className=" hover:underline">
+                    NextJS
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    React JS
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Tailwind CSS
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Laravel
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
+        </div>
+        <div className="px-4 py-6 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-300 sm:text-center">
+            © 2025 <a href="/">Aiman Yusuf Wicaksono</a>. All Rights Reserved.
+          </span>
         </div>
       </div>
     </footer>
