0a29c07f1ed6024dc7fd90864ed0e5f556a66afa
aimanyusufw <aimanyusufdev@gmail.com>
2025-04-10 01:01:53 +0700

    fix : fix build error at img tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from "@/components/sections/AboutMe";
 import FAQ from "@/components/sections/FAQ";
 import Hero from "@/components/sections/Hero";
 import Portfolio from "@/components/sections/Portfolio";
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
@@ -19,35 +20,35 @@ const Home = () => {
             and scalable solutions
           </p>
           <div className="mx-auto mt-14 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <img
+            <Image
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="img/nextjs.svg"
               alt="Reform"
               width="158"
               height="48"
             />
-            <img
+            <Image
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="img/laravel.svg"
               alt="Transistor"
               width="158"
               height="48"
             />
-            <img
+            <Image
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="img/tailwind.svg"
               alt="Tuple"
               width="158"
               height="48"
             />
-            <img
+            <Image
               className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
               src="img/react.svg"
               alt="SavvyCal"
               width="158"
               height="48"
             />
-            <img
+            <Image
               className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
               src="img/stripe.svg"
               alt="Statamic"
