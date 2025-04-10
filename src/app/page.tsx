242a1fae886c7368773b65410410bfb1f98a3683
aimanyusufw <aimanyusufdev@gmail.com>
2025-04-10 07:57:09 +0700

    add : add Article section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import ArticleCard from "@/components/cards/ArticleCard";
 import AboutMe from "@/components/sections/AboutMe";
-import FAQ from "@/components/sections/FAQ";
 import Hero from "@/components/sections/Hero";
 import Portfolio from "@/components/sections/Portfolio";
+import { articles } from "@/data/home";
 import Image from "next/image";
 import React from "react";
 
@@ -59,7 +60,21 @@ const Home = () => {
         </div>
       </section>
       <Portfolio />
-      <FAQ />
+      <div className="py-24 sm:py-32">
+        <div className="container">
+          <h2 className="text-base/7 font-semibold text-red-500">
+            From the blog
+          </h2>
+          <p className="mt-2 max-w-lg text-balance capitalize text-4xl font-semibold tracking-tight sm:text-5xl">
+            Learn how to grow be developers.
+          </p>
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {articles.map((data) => (
+              <ArticleCard data={data} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/cards/ArticleCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type ArticleData = {
+  image: string;
+  date: string;
+  author: string;
+  title: string;
+};
+
+type ArticleCardProps = {
+  data: ArticleData;
+};
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ data }) => {
+  return (
+    <article className="h-[450px] rounded-xl overflow-hidden shadow-lg bg-white relative">
+      <img
+        className="w-full h-full object-cover"
+        src={data.image}
+        alt={`Cover for ${data.title}`}
+      />
+      <div className="absolute inset-0 w-full p-10 text-white bg-gradient-to-t from-black/90 to-transparent flex flex-col justify-end">
+        <p className="text-sm opacity-80">
+          {data.date} • <span className="font-medium">{data.author}</span>
+        </p>
+        <h3 className="text-lg font-semibold">{data.title}</h3>
+      </div>
+    </article>
+  );
+};
+
+export default ArticleCard;
