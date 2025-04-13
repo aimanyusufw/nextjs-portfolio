73520298cc718cd14496fb4285c0988afca855db
aimanyusufw <aimanyusufdev@gmail.com>
2025-04-13 16:22:12 +0700

    ref : refacto layout

--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -14,33 +14,24 @@ const FAQ = () => {
   };
 
   return (
-    <section className="py-24">
-      <div className="container py-12">
-        <div className="md:px-4 grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-          <div className="space-y-6 md:space-y-8 mb-10 md:mb-0">
-            <h1 className="font-medium capitalize text-4xl md:text-5xl md:leading-relaxed max-w-sm md:max-w-md">
-              frequently asked questions{" "}
-            </h1>
-            <h3 className="font-medium font-firaCode">My Social Media ⮧</h3>
-            <div className="flex gap-4">
-              {socialMedia.map((data) => (
-                <Link href={data.url} key={data.name} target="_blank">
-                  <data.icon size={20} className="w-4 md:w-5" />
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="space-y-4">
-            {accordionItems.map((data, index) => (
-              <Accordion
-                data={data}
-                isOpen={accordionOpen === index}
-                setIsOpen={handleButton}
-                index={index}
-                key={index}
-              />
-            ))}
-          </div>
+    <section className="py-24 sm:py-32">
+      <div className="container">
+        <h2 className="text-base/7 text-center font-semibold text-red-500 mb-2">
+          Frequently Asked Questions
+        </h2>
+        <p className="mb-10 max-w-lg mx-auto text-center text-balance capitalize text-4xl font-semibold tracking-tight sm:text-5xl">
+          Learn how to grow be developers.
+        </p>
+        <div className="space-y-4  w-full md:w-1/2 mx-auto">
+          {accordionItems.map((data, index) => (
+            <Accordion
+              data={data}
+              isOpen={accordionOpen === index}
+              setIsOpen={handleButton}
+              index={index}
+              key={index}
+            />
+          ))}
         </div>
       </div>
     </section>
